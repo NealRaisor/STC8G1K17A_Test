ed13110a7565cc51cc89dbb24eb04e0ba922d81e
Neal Raisor <139026578+NealRaisor@users.noreply.github.com>
2023-09-04 13:22:07 +0800

    Update STC8G_H_Timer_Isr.c
    Signed-off-by: Neal Raisor <139026578+NealRaisor@users.noreply.github.com>

--- I2C_OLED_Test/src/STC8G_H_Timer_Isr.c
+++ I2C_OLED_Test/src/STC8G_H_Timer_Isr.c
@@ -1,8 +1,6 @@
 #include "STC8G_H_Timer.h"
 #include "STC8G_H_UART.h"
 
-// extern u8 AHT21_Delay;
-// extern u16 KEY_Recorder;
 extern u16 ms_Recorder;
 extern bit Flag_1s; // 1s
 
@@ -22,15 +20,6 @@ void Timer0_ISR_Handler(void) interrupt TMR0_VECTOR
     if (COM1.RX_TimeOut > 0) {
         --COM1.RX_TimeOut;
     }
-    // // AHT21延时
-
-    // if (AHT21_Delay > 0) {
-    //     --AHT21_Delay;
-    // }
-
-    // if (KEY0 == 0) {
-    //     ++KEY_Recorder;
-    // }
 }
 #endif
 
